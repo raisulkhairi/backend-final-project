6b203dab4693a1fd3622017dbb519f7ec70a84c3
unknown <raisulkhairi20@gmail.com>
2022-02-11 09:05:27 +0700

    first commit

--- database/connectDB.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-require("dotenv").config();
-console.log(process.env.CONNECT_DB);
-module.exports = mongoose
-  .connect(process.env.CONNECT_DB)
-  .then(() => console.log("Database connected successfully"))
-  .catch(() => {
-    console.log("Database is not connected");
-  });
--- server.js
+++
@@ -1,7 +0,0 @@
-const app = require("./app");
-require("dotenv").config();
-const port = process.env.PORT;
-
-app.listen(port, () => {
-  console.log("Server is listening on port " + port);
-});
